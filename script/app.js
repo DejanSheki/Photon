9864598ba5a11ef9847295636081cab644dd12de
Dejan <dejan.sheki.lukic@gmail.com>
2021-05-21 22:30:27 +0200

    ....

--- script/app.js
+++ script/app.js
@@ -1,3 +1,28 @@
-var key = config.key;
 
-const auth = "d5eb13aa8fmshf8dbaeecb666520p1ac7fbjsn715255f24c56";
+const auth = '563492ad6f9170000100000160af47e4db6b4dea9574e47635a5c6fd';
+const gallery = document.querySelector('.gallery');
+const searchInput = document.querySelector('.search-input');
+const submitButton = document.querySelector('.submit-btn');
+let searchValue;
+
+async function curatedPhotos() {
+    const dataFetch = await fetch("https://api.pexels.com/v1/curated?per_page=15", {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            Authorization: auth
+        }
+    });
+    const data = await dataFetch.json();
+    console.log(data);
+    data.photos.forEach(photo => {
+        const galleryImg = document.createElement('div');
+        galleryImg.classList.add('gallery-img');
+        galleryImg.innerHTML = `<img src=${photo.src.large}> </img>
+        <p>${photo.photographer}</p>
+        `;
+        gallery.appendChild(galleryImg);
+    });
+}
+
+curatedPhotos();
