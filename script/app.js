44e8abeab22f91a2d52600c2b22f85c7ed058e64
Dejan <dejan.sheki.lukic@gmail.com>
2021-05-25 16:42:18 +0200

    fetch, search...

--- script/app.js
+++ script/app.js
@@ -2,11 +2,21 @@
 const auth = '563492ad6f9170000100000160af47e4db6b4dea9574e47635a5c6fd';
 const gallery = document.querySelector('.gallery');
 const searchInput = document.querySelector('.search-input');
-const submitButton = document.querySelector('.submit-btn');
+const form = document.querySelector('.search-form');
 let searchValue;
 
-async function curatedPhotos() {
-    const dataFetch = await fetch("https://api.pexels.com/v1/curated?per_page=15", {
+searchInput.addEventListener('input', updateInput);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    searchPhotos(searchValue);
+});
+
+function updateInput(e) {
+    searchValue = e.target.value;
+}
+
+async function fetchApi(url) {
+    const dataFetch = await fetch(url, {
         method: 'GET',
         headers: {
             Accept: 'application/json',
@@ -14,18 +24,28 @@ async function curatedPhotos() {
         }
     });
     const data = await dataFetch.json();
-    console.log(data);
+    return data;
+}
+
+function generatePictures(data) {
     data.photos.forEach(photo => {
         const galleryImg = document.createElement('div');
         galleryImg.classList.add('gallery-img');
         galleryImg.innerHTML = `<img src=${photo.src.large}> </img>
-        <p>${photo.photographer}</p>
-        `;
+        <p>${photo.photographer}</p>`;
         gallery.appendChild(galleryImg);
     });
 }
 
+async function curatedPhotos() {
+    const data = await fetchApi("https://api.pexels.com/v1/curated?per_page=15&page=1");
+    generatePictures(data);
+}
+
+async function searchPhotos(query) {
+    const data = await fetchApi(`https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`);
+    generatePictures(data);
+}
+
 curatedPhotos();
 
-
-const test = test;
